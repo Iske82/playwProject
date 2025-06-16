c1af292eb251da2992578b5d18dcfd62ca7a9b10
Iske82 <isidor.ramljak@gmail.com>
2025-06-16 14:18:38 +0200

    Modify config TS

--- customs/playwright.config.ts
+++ customs/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    storageState: "./LoginAuth.json",
+    storageState: "./loginAuth.json",
   },
 
   /* Configure projects for major browsers */
